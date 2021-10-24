ce60ceece6868839c33c41edb5eea11bf24f4970
Travis CI <travis@travis-ci.org>
2021-10-24 18:15:08 +0200

    fix: xml export supports unwrapped response

--- src/Macros/JsonResponse/ToXml.php
+++ src/Macros/JsonResponse/ToXml.php
@@ -4,6 +4,7 @@ namespace audunru\ExportResponse\Macros\JsonResponse;
 
 use audunru\ExportResponse\Enums\MimeType;
 use Illuminate\Http\Response;
+use Illuminate\Support\Arr;
 use Spatie\ArrayToXml\ArrayToXml;
 
 class ToXml
@@ -12,6 +13,7 @@ class ToXml
     {
         return function (string $filename): Response {
             $data = $this->getData(true);
+            $data = Arr::isAssoc($data) ? $data : ['data' => $data];
             $xml = ArrayToXml::convert($data);
 
             return (new Response($xml, Response::HTTP_OK, [
